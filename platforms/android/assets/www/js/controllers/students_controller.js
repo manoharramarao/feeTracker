ea0e6888704e0bd6e8ab94583b05476a85c3a81c
Manohar <manoharramarao@gmail.com>
2016-11-14 00:02:00 +0100

    beautified students page

--- platforms/android/assets/www/js/controllers/students_controller.js
+++ platforms/android/assets/www/js/controllers/students_controller.js
@@ -6,12 +6,21 @@ app.controller("StudentsController", function($scope, $ionicPlatform, $ionicLoad
         $ionicLoading.show({
             template: "Loading..."
         });
-        var query = "SELECT id, first_name FROM students";
+        var query = "SELECT id, first_name, last_name, gender FROM students";
         db.transaction(function(tx){
             tx.executeSql(query, [], function(tx, res){
                 if(res.rows.length > 0){
                     for(var i=0; i<res.rows.length; i++){
-                        $scope.students.push({id: res.rows.item(i).id, first_name: res.rows.item(i).first_name});
+                        /*$scope.students.push({id: res.rows.item(i).id, first_name: res.rows.item(i).first_name,
+                        last_name: res.rows.item(i).last_name, gender: res.rows.item(i).gender});*/
+                        // TODO: @Manohar - Need to remove this if condition once you same profile pics
+                        if(res.rows.item(i).gender === "Male"){
+                            $scope.students.push({id: res.rows.item(i).id, first_name: res.rows.item(i).first_name,
+                                last_name: res.rows.item(i).last_name, gender: res.rows.item(i).gender, icon: 'img/male.png'});
+                        }else if(res.rows.item(i).gender === "Female"){
+                            $scope.students.push({id: res.rows.item(i).id, first_name: res.rows.item(i).first_name,
+                                last_name: res.rows.item(i).last_name, gender: res.rows.item(i).gender, icon: 'img/female.png'});
+                        }
                     }
                 };
                 console.log("students = " + JSON.stringify($scope.students));
